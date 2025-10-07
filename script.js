9c7e1454f0c05de7a34ec8069dd53adb7411e588
Rhapsodysss <ahzazean0@gmail.com>
2025-10-07 12:05:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const tombolSapa = document.getElementById('sapaButton');
 tombolSapa.addEventListener('click',function(){
   alert('Thankyou for Fiviting :)');
 });
+
+const tombodashboard = document.getElementById('dashboard');
+tomboldashboard.addEventListener('click',function(){
+  alert('Heading to Dashboard');
+});
